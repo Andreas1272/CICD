3d216f327fe89ab31770b7f472a8e4947992049c
Andreas Bogner <andreas.bogner@bmw.de>
2024-12-09 08:45:12 +0100

    Remove Test.txt and implement basic structure

--- main.cpp
+++ main.cpp
@@ -1,8 +1,44 @@
 #include <iostream>
+#include <string>
+
+class Ressource
+{
+    public:
+
+    // tbd: Constructor
+
+    ~Ressource() = default;
+
+    void AddRessource(std::string& ressource_name)
+    {
+        // tbd
+    }
+
+    void EditRessource(std::string& ressource_name)
+    {
+        // tbd
+    }
+
+    void DeleteRessource(std::string& ressource_name)
+    {
+        // tbd
+    }
+
+    void ShowUser(std::string& ressource_name)
+    {
+        // tbd
+    }
+
+    private:
+    std::string m_ressource_name;
+    std::string m_ressource_user;
+
+
+};
+
+
 
 int main() {
-    for (int i=1; i<11; i++){
-        std::cout << "Hello" << std::endl;
-    }
+    // tbd
     return 0;
 }
