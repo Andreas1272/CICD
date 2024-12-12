a7cb6115d074c778acd862b8b02792db3fbe40f4
Andreas Bogner <andreas.bogner@bmw.de>
2024-12-12 08:04:40 +0100

    Implement basic functionality for printers

--- main.cpp
+++ main.cpp
@@ -1,44 +1,81 @@
 #include <iostream>
+#include <ostream>
 #include <string>
 
 class Ressource
 {
     public:
 
-    // tbd: Constructor
+    // tbd: Constructor?
 
     ~Ressource() = default;
 
-    void AddRessource(std::string& ressource_name)
+    //virtual void AddRessource(std::string ressource_name) = 0;
+    //virtual void EditRessource(std::string ressource_name) = 0;
+    //virtual void DeleteRessource(std::string ressource_name) = 0;
+    virtual void SetRessourceName(std::string ressource_name) = 0;
+    virtual std::string GetRessourceName() = 0;
+    virtual void SetRessourceUser(std::string ressource_user) = 0;
+    virtual std::string GetRessourceUser() = 0;
+    virtual bool GetRessourceInUse() = 0;
+};
+
+class Printer : public Ressource {
+    public:
+    Printer(std::string ressource_name, std::string ressource_user): m_ressource_name{ressource_name}, m_ressource_user{ressource_user} {};
+
+    ~Printer() = default;
+
+    void SetRessourceName(std::string ressource_name)
     {
-        // tbd
+        m_ressource_name = ressource_name;
     }
 
-    void EditRessource(std::string& ressource_name)
+    std::string GetRessourceName()
     {
-        // tbd
+        return m_ressource_name;
     }
 
-    void DeleteRessource(std::string& ressource_name)
+    void SetRessourceUser(std::string ressource_user)
     {
-        // tbd
+        m_ressource_user = ressource_user;
     }
 
-    void ShowUser(std::string& ressource_name)
+    std::string GetRessourceUser()
     {
-        // tbd
+        return m_ressource_user;
+    }
+
+    bool GetRessourceInUse()
+    {
+        if(m_ressource_user.compare("") == 0)
+        {
+            return false;
+        }
+        else
+        {
+            return true;
+        }
     }
 
     private:
     std::string m_ressource_name;
     std::string m_ressource_user;
 
-
 };
-
-
 
 int main() {
     // tbd
+    Printer printer_1("Printer_1","");
+    std::cout << "Name: " << printer_1.GetRessourceName() << std::endl;
+    std::cout << "User: " << printer_1.GetRessourceUser() << std::endl;
+    std::cout << "In use: " << printer_1.GetRessourceInUse() << std::endl;
+
+    printer_1.SetRessourceUser("EF-724");
+    std::cout << "Name: " << printer_1.GetRessourceName() << std::endl;
+    std::cout << "User: " << printer_1.GetRessourceUser() << std::endl;
+    std::cout << "In use: " << printer_1.GetRessourceInUse() << std::endl;
+
+
     return 0;
 }
